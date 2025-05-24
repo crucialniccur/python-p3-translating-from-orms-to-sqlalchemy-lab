e2ed0d67b49c999245f301f3e8e4befe5529ccff
Mati Joseph <crucialniccur@gmail.com>
2025-05-24 14:05:10 +0300

    define the find_by_name_and_breed

--- lib/dog.py
+++ lib/dog.py
@@ -30,7 +30,7 @@ def find_by_id(session, id):
 
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(dog_name=name, dog_breed=breed).first()
 
 
 def update_breed(session, dog, breed):
