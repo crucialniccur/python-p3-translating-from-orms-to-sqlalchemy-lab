2b5e1756c7bea7de1a66f21a087c39fd41e14c3d
Mati Joseph <crucialniccur@gmail.com>
2025-05-24 14:02:32 +0300

    import create_engine from sqlachemy and define the create_table function

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,35 @@
+from sqlalchemy import create_engine
+
 from models import Dog
 
+
+engine = create_engine('sqlite:///memory:')
+
+
 def create_table(base):
-    pass
+    base.metadata.create_all(engine)
+    return engine
+
 
 def save(session, dog):
     pass
 
+
 def get_all(session):
     pass
+
 
 def find_by_name(session, name):
     pass
 
+
 def find_by_id(session, id):
     pass
 
+
 def find_by_name_and_breed(session, name, breed):
     pass
+
 
 def update_breed(session, dog, breed):
     pass
